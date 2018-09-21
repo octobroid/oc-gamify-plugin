ceaa1f338fc3e27af0263920b8ddf8af94396f6e
syehan <muhammad.syehan@gmail.com>
2018-09-21 11:33:31 +0700

    first commit

---
+++ Plugin.php
@@ -0,0 +1,96 @@
+<?php namespace Octobro\Gamify;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Gamify Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Gamify',
+            'description' => 'No description provided yet...',
+            'author'      => 'Octobro',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Octobro\Gamify\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'octobro.gamify.some_permission' => [
+                'tab' => 'Gamify',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'gamify' => [
+                'label'       => 'Gamify',
+                'url'         => Backend::url('octobro/gamify/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['octobro.gamify.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
