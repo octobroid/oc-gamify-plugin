28ff802a47c80ee08eb3d714b7ca4cc698e6ec9e
syehan <muhammad.syehan@gmail.com>
2018-09-21 11:51:27 +0700

    create Point,level,Leaderboard Log and level model

---
+++ models/LeaderBoardLog.php
@@ -0,0 +1,37 @@
+<?php namespace Octobro\Gamify\Models;
+
+use Model;
+
+/**
+ * LeaderBoardLog Model
+ */
+class LeaderBoardLog extends Model
+{
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'octobro_gamify_leader_board_logs';
+
+    /**
+     * @var array Guarded fields
+     */
+    protected $guarded = ['*'];
+
+    /**
+     * @var array Fillable fields
+     */
+    protected $fillable = [];
+
+    /**
+     * @var array Relations
+     */
+    public $hasOne = [];
+    public $hasMany = [];
+    public $belongsTo = [];
+    public $belongsToMany = [];
+    public $morphTo = [];
+    public $morphOne = [];
+    public $morphMany = [];
+    public $attachOne = [];
+    public $attachMany = [];
+}
