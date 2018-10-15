1d45fc5f9eb32d6560f622951e9c73d1eac87d3e
Trias Nur Rahman <contact@triasrahman.com>
2018-10-15 16:21:26 +0700

    Add code column to missions table

---
+++ updates/add_code_column_to_missions_table.php
@@ -0,0 +1,24 @@
+<?php namespace Octobro\Gamify\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        if (Schema::hasTable('octobro_gamify_missions')) {
+            Schema::table('octobro_gamify_missions', function(Blueprint $table) {
+                $table->string('code')->after('name');
+            });
+        }
+    }
+
+    public function down()
+    {
+        Schema::table('octobro_gamify_missions', function(Blueprint $table) {
+            $table->dropColumn('code');
+        });
+    }
+}
