daaf18c6e910645f32c59daf8ce50a6f79540193
Prasetyo Iman Nugroho <pras_iman@hotmail.com>
2018-10-16 14:16:11 +0700

    update leaderboard backend

--- controllers/LeaderboardLogs.php
+++ controllers/LeaderboardLogs.php
@@ -2,6 +2,7 @@
 
 use BackendMenu;
 use Backend\Classes\Controller;
+use Octobro\Gamify\Models\LeaderboardLog;
 
 /**
  * Leaderboard Logs Back-end Controller
@@ -21,5 +22,12 @@ class LeaderboardLogs extends Controller
         parent::__construct();
 
         BackendMenu::setContext('Octobro.Gamify', 'gamify', 'leaderboardlogs');
+    }
+
+    public function preview($leaderboardId = null)
+    {
+        $this->vars['leaderboardLog'] = $leaderboardLog = LeaderboardLog::find($leaderboardId);
+
+        return $this->asExtension('FormController')->preview($leaderboardId);
     }
 }
