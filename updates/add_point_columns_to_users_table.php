dfb81baa7ca7d9a309e161150c67f7dba32381dc
Trias Nur Rahman <contact@triasrahman.com>
2018-10-20 00:53:38 +0700

    Fix wrong migration 😤

--- updates/add_point_columns_to_users_table.php
+++ updates/add_point_columns_to_users_table.php
@@ -4,7 +4,7 @@ use Schema;
 use October\Rain\Database\Schema\Blueprint;
 use October\Rain\Database\Updates\Migration;
 
-class CreateUsersTable extends Migration
+class AddPointColumnsToUsersTable extends Migration
 {
     public function up()
     {
