d30a1771186b909b7d520df3f8bde64751dbf23d
Prasetyo Iman Nugroho <pras_iman@hotmail.com>
2018-10-19 17:12:27 +0700

    update mission model

--- models/Mission.php
+++ models/Mission.php
@@ -12,9 +12,9 @@ class Mission extends Model
     public $rules = [
         'name'                  => 'required|between:4,30',
         'points'                => 'required|integer',
-        'code'                  => 'required',
-        'type'                  => 'required|string:daily|weekly|one-time|always',
-        'target'                => 'required'
+        'code'                  => 'required|string',
+        'type'                  => 'required|string',
+        'target'                => 'required|integer'
     ];
     /**
      * @var string The database table used by the model.
