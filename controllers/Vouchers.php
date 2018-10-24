c2ccfe5e3805f149867e0bbdfb70567cc1674ca5
Prasetyo Iman Nugroho <pras_iman@hotmail.com>
2018-10-24 17:09:52 +0700

    add downloadable QR code to voucher

--- controllers/Vouchers.php
+++ controllers/Vouchers.php
@@ -2,6 +2,7 @@
 
 use BackendMenu;
 use Backend\Classes\Controller;
+use SimpleSoftwareIO\QrCode\BaconQrCodeGenerator as QrCode;
 
 /**
  * Vouchers Back-end Controller
@@ -21,5 +22,25 @@ class Vouchers extends Controller
         parent::__construct();
 
         BackendMenu::setContext('Octobro.Gamify', 'gamify', 'vouchers');
+    }
+
+    public function downloadQR($code)
+    {
+        $qrCode = new QrCode();
+        $decoded = $qrCode->format('png')->merge('\logo.png', .1)->encoding('UTF-8')->size(1000)->generate($code);
+        $file = "$code-QRcode.png";
+        file_put_contents($file, $decoded);
+        if (file_exists($file)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: image/png');
+            header('Content-Disposition: attachment; filename="'.basename($file).'"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            readfile($file);
+            unlink($file);
+            exit;
+        }
     }
 }
